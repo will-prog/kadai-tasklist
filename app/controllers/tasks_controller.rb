2a1a2ce7067736f97ad81d14a4a2d7871569d021
will.programming <will.world.traveler.programming@gmail.com>
2020-02-27 09:01:12 +0000

    CRUD tasks

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,53 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+    
+  def show
+    @task = Task.find(params[:id])
+  end
+  
+  def new
+    @task = Task.new
+  end
+  
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      flash[:success] = 'タスクが作成されました。'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'タスクが作成されませんでした。'
+      render :new
+    end
+  end
+  
+  def edit
+    @task = Task.find(params[:id])
+  end
+  
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      flash[:success] = 'タスクは編集されました。'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'タスクは編集されませんでした。'
+      render :edit
+    end
+  end
+  
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    
+    flash[:success] = 'タスクは削除されました。'
+    redirect_to tasks_url
+  end
+  
+  private
+  #Strong parameter
+  def task_params
+    params.require(:task).permit(:content)
+  end
+end

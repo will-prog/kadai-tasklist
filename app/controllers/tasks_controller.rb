0adb6688c1328741be16bd157f8e5845ff6bda15
will.programming <will.world.traveler.programming@gmail.com>
2020-02-28 03:53:38 +0000

    DRY

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  
   def index
     @tasks = Task.all
   end
     
   def show
-    @task = Task.find(params[:id])
   end
   
   def new
@@ -23,11 +24,9 @@ class TasksController < ApplicationController
   end
   
   def edit
-    @task = Task.find(params[:id])
   end
   
   def update
-    @task = Task.find(params[:id])
     if @task.update(task_params)
       flash[:success] = 'タスクは編集されました。'
       redirect_to @task
@@ -38,7 +37,6 @@ class TasksController < ApplicationController
   end
   
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     
     flash[:success] = 'タスクは削除されました。'
@@ -46,6 +44,12 @@ class TasksController < ApplicationController
   end
   
   private
+  
+  #DRY - Don't Repeat Yourself
+  def set_task
+    @task = Task.find(params[:id])
+  end
+  
   #Strong parameter
   def task_params
     params.require(:task).permit(:content)
